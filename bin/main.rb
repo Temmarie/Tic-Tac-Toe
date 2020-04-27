8dcf9b4dabfeb7eccacf08f0a1c57219c276192f
Osong Agberndifor <agberndifor7@gmail.com>
2020-04-27 08:51:06 +0100

    Add default values for players

--- bin/main.rb
+++ bin/main.rb
@@ -13,11 +13,23 @@ puts "\t X | O | X"
 puts
 
 
-print 'Player One name please: '
-players1 = gets.chomp
+print "Player One press \'E'\ to enter name or \'G'\ to play as guest: "
+id = gets.chomp.upcase
+if id == 'G'
+  players1 = 'Guest__01'
+elsif id == 'E'
+  print 'Player One name please: '
+  players1 = gets.chomp
+end
 puts
-print 'Player Two name please: '
-players2 = gets.chomp
+print "Player Two press \'E'\ to enter name or \'G'\ to play as guest: "
+id2 = gets.chomp.upcase
+if id2 == 'G'
+  players2 = 'Guest__02'
+elsif id2 == 'E'
+  print 'Player Two name please: '
+  players2 = gets.chomp
+end
 puts
 player1 = Players.new(players1, 'X')
 player2 = Players.new(players2, 'O')
@@ -28,15 +40,17 @@ puts
 puts 'This is your playing board'
 board = Board.new
 puts board.display_board
+puts
 
 playerz = [player1, player2]
 
 3.times do
   playerz.each do |playa|
-    puts "#{playa.name} choose a cell"
+    print "#{playa.name} choose a cell: "
     input = gets.chomp.to_i
     board.positions[input - 1] = playa.char
     puts board.display_board
+    puts
   end
 end
 
