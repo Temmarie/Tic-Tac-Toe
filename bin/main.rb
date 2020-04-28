f7c93582b4da48fc1a388fe08c6d8114d6f61083
Osong Agberndifor <agberndifor7@gmail.com>
2020-04-28 13:57:17 +0100

    Add function to keep plaing if players decide to or stop if they do not want to keep playing

--- bin/main.rb
+++ bin/main.rb
@@ -11,86 +11,95 @@ puts "\t---|---|---"
 puts "\t X | O | X"
 puts
 
-print "Player One press \'P'\ to enter name or \'Enter'\ to play as guest: "
-id = gets.chomp.upcase
-if id == 'P'
-  print 'Player One name please: '
-  players1 = gets.chomp
-else
-  players1 = 'Guest__01'
-end
-puts
-print "Player Two press \'P'\ to enter name or \'Enter'\ to play as guest: "
-id2 = gets.chomp.upcase
-if id2 == 'P'
-  print 'Player Two name please: '
-  players2 = gets.chomp
-else
-  players2 = 'Guest__02'
-end
-puts
+game = true
 
-player1 = Players.new(players1, 'X')
-player2 = Players.new(players2, 'O')
-puts "#{player1.name} your symbol is #{player1.char}"
-puts
-puts "#{player2.name} your symbol is #{player2.char}"
-puts
-puts 'This is your playing board'
-board = Board.new
-puts board.display_board
-puts
-
-playerz = [player1, player2]
-
-game_on = true
-
-while game_on
-  playerz.each do |playa|
-    if game_on
-
-      loop do
-        print "#{playa.name} choose a cell: "
-        input = gets.chomp.to_i
-        inputs = input - 1
-
-        if board.positions.include?(input)
-          playa.player_input << inputs
-          board.positions[inputs] = playa.char
-          break
-        else
-          puts 'Invalid move/character try again!'
-          puts
-          next
-        end
-      end
-      puts board.display_board
-      puts
-    end
-    if board.won?(playa.player_input)
-      puts "#{playa.name} Wins!!!"
-
-      game_on = false
-      break
-
-    elsif board.positions.none?(Integer)
-      puts 'Game is Draw'
-
-      game_on = false
-      break
-    end
+while game
+  print "Player One press \'P'\ to enter name or \'Enter'\ to play as guest: "
+  id = gets.chomp.upcase
+  if id == 'P'
+    print 'Player One name please: '
+    players1 = gets.chomp
+  else
+    players1 = 'Guest__01'
   end
+  puts
+  print "Player Two press \'P'\ to enter name or \'Enter'\ to play as guest: "
+  id2 = gets.chomp.upcase
+  if id2 == 'P'
+    print 'Player Two name please: '
+    players2 = gets.chomp
+  else
+    players2 = 'Guest__02'
+  end
+  puts
 
-  next if game_on
+  player1 = Players.new(players1, 'X')
+  player2 = Players.new(players2, 'O')
+  puts "#{player1.name} your symbol is #{player1.char}"
+  puts
+  puts "#{player2.name} your symbol is #{player2.char}"
+  puts
+  puts 'This is your playing board'
+  board = Board.new
+  puts board.display_board
+  puts
 
-  print "\n Do you want to play again? [yes(y) / no(n)]: "
-  play_again = gets.chomp.downcase
+  playerz = [player1, player2]
 
-  if play_again == 'yes' or play_again == 'y'
-    puts "\n Sorry not available yet, this is the beta version, new updates coming :)"
-    break
-  elsif play_again == 'no' or play_again == 'n'
-    puts 'Thanks for playing!'
+  game_on = true
+
+  while game_on
+    playerz.each do |playa|
+      if game_on
+
+        loop do
+          print "#{playa.name} choose a cell: "
+          input = gets.chomp.to_i
+          inputs = input - 1
+
+          if board.positions.include?(input)
+            playa.player_input << inputs
+            board.positions[inputs] = playa.char
+            break
+          else
+            puts 'Invalid move/character try again!'
+            puts
+            next
+          end
+        end
+        puts board.display_board
+        puts
+      end
+      if board.won?(playa.player_input)
+        puts "#{playa.name} Wins!!!"
+
+        game_on = false
+        break
+
+      elsif board.positions.none?(Integer)
+        puts 'Game is Draw'
+
+        game_on = false
+        break
+      end
+    end
+    game = false
+  end
+  next if game
+
+  puts
+  print 'Want to play again? [yes(y) or no(n)]: '
+  value = gets.chomp
+  puts
+
+  if value == 'yes' || value == 'y'
+    system('cls') || system('clear')
+    game = true
+    next
+  else
+    puts
+    puts 'Thanks for playing'
+    game = false
     break
   end
 end
