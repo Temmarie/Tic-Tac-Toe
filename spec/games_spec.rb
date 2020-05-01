4b1d676363e5a05f47f6ec986fd4dec2ef2e495a
Osong Agberndifor <agberndifor7@gmail.com>
2020-05-01 14:30:48 +0100

    Fix testing logic

--- spec/games_spec.rb
+++ spec/games_spec.rb
@@ -1,57 +1,42 @@
 require './lib/players.rb'
 require './lib/board.rb'
 
-describe Board do
+RSpec.describe Board do
   let(:tictactoe) { Board.new }
   let(:player) { Players.new('Grace', 'X') }
+  let(:arr) do
+    [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6],
+      [0, 4, 8],
+      [1, 4, 7]
+    ]
+  end
 
   describe '#won?' do
-    it 'returns true if the players input includes any of the winning combination' do
-      arr = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [2, 4, 6],
-        [0, 4, 8],
-        [1, 4, 7]
-      ]
-
+    it 'returns true if the players input includes any of the winning combinations' do
       input = [1, 3, 2, 0, 5]
 
-      arr.each do |ele|
-        true if ele.all? { |x| input.include?(x) }
-        expect(tictactoe.won?(ele)).to eql(true)
-      end
+      arr.each { |sub_arr| sub_arr.all? { |ele| input.include?(ele) } }
+      expect(tictactoe.won?(input)).to eql(true)
+    end
 
-    it 'returns false if the players input does not include any of the winning combination' do
-      arr = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [2, 4, 6],
-        [0, 4, 8],
-        [1, 4, 7]
-      ]
+    it 'returns false if the players input does not include any of the winning combinations' do
+      input = [8, 5, 7, 1]
 
-      input = [9, 8, 4, 0, 1]
-
-      arr.each do |ele|
-        true if ele.all? { |x| input.include?(x) }
-        expect(tictactoe.won?(ele)).to eql(false)
-      end
+      arr.each { |sub_arr| sub_arr.all? { |ele| input.include?(ele) } }
+      expect(tictactoe.won?(input)).to eql(false)
     end
   end
-  
 
   describe '#valid_input?' do
     it 'returns true if the user input is included in the board positions' do
-      (1..9).each do |ele|
+      tictactoe.positions.each do |ele|
         expect(tictactoe.valid_input?(ele)).to eql(true)
       end
     end
@@ -64,7 +49,7 @@ describe Board do
   end
 
   describe '#update_board' do
-    it 'returns true and updates the board if the user input is valid and matches a position on the board' do
+    it 'Updates board with user character if input is valid and position is available' do
       tictactoe.update_board(player, 1)
       expect(tictactoe.positions[1]).to eql('X')
     end
@@ -72,17 +57,7 @@ describe Board do
 
   describe '#draw?' do
     it 'returns true when the game has not been won and there are no integers on the board' do
-      # 9.times do |char|
-      #   tictactoe.update_board(player, char)
-      # end
-      # expect(tictactoe.draw?).to eql(true)
-      if tictactoe.!won? do 
-        expect(tictactoe.draw?).to eql(true)
-      end
-
-      if positions.none?(Integer) do 
-        expect(tictactoe.draw?).to eql(true)
-      end
+      expect(tictactoe.draw?).to be true if tictactoe.positions.all?(String) && !tictactoe.won?
     end
   end
 end
